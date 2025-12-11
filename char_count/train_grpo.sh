fc2757bb52f11c8304312ed16d375abbf9443c03
Chi Zhang <czhangseu@gmail.com>
2025-12-11 10:37:04 +0800

    update

--- char_count/train_grpo.sh
+++ char_count/train_grpo.sh
@@ -1,5 +1,10 @@
 set -x
 
+export RAY_DEBUG=legacy
+export TORCHINDUCTOR_COMPILE_THREADS=1
+
+use_legacy_worker_impl=disable
+EXP_NAME=smol135m_grpo-1210a1-legacy-$use_legacy_worker_impl
 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
@@ -10,13 +15,14 @@ python3 -m verl.trainer.main_ppo \
     data.max_response_length=128 \
     data.filter_overlong_prompts=False \
     data.truncation='error' \
+    data.dataloader_num_workers=0 \
     actor_rollout_ref.model.path=$HOME/experiments/char_count/models/sft/megatron/global_step_140/huggingface \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=5000 \
-    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.0 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
@@ -25,21 +31,25 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.enforce_eager=True \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=10000 \
+    actor_rollout_ref.rollout.agent.num_workers=1 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.ref.log_prob_use_dynamic_bsz=True \
+    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=10000 \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger='["console","tensorboard"]' \
     trainer.project_name='verl_example' \
-    trainer.experiment_name='smol135m_grpo-1128a1' \
+    trainer.experiment_name=$EXP_NAME \
     trainer.val_before_train=True \
     trainer.n_gpus_per_node=1 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.test_freq=5 \
     trainer.total_epochs=5 \
-    trainer.use_legacy_worker_impl=disable \
+    trainer.use_legacy_worker_impl=$use_legacy_worker_impl \
     custom_reward_function.path=./reward_function.py \
     custom_reward_function.name=char_count_reward_function
